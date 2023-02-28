2bc0e59581fc8a6c641843cf50b6363dda25f647
Lucas Wenck <lucas.wenck@gmail.com>
2023-02-27 21:38:48 -0300

    Modify script to show the code of the key and a phrase

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const content = document.querySelector('#main_content');
 const clarification = document.createElement('h3');
 clarification.setAttribute('id', 'message');
 clarification.textContent = ``;
-clarification.style.alignSelf = `center`;
 clarification.style.fontSize = `32px`;
 content.appendChild(clarification);
 window.addEventListener('keydown', function(e){
     const keycode = e.keyCode;
-    console.log(keycode);
-    key.style.fontSize = `200px`;
-    document.getElementById('key').innerText = `${keycode}`;
+    const keyPress = e.code;
+    console.log(e);
+    key.style.fontSize = `100px`;
+    document.getElementById('key').innerText = `The keycode of the ${keyPress} is ${keycode}`;
     this.document.getElementById('message').innerText = `This is the keycode of the key you just pressed!`;
 })
